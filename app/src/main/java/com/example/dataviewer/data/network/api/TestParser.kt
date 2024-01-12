c75eb8f3b4e199650d49f1608ca7322f2fdc1762
George Dashkin <siodo@mail.ru>
2024-01-12 18:08:41 +0300

    12_01_2024

--- app/src/main/java/com/example/dataviewer/data/network/api/TestParser.kt
+++ app/src/main/java/com/example/dataviewer/data/network/api/TestParser.kt
@@ -1,8 +1,44 @@
 package com.example.dataviewer.data.network.api
 
 fun main() {
-    val mockFeed =
-        "<offer id=\"982045\" available=\"true\"><url>https://www.citilink.ru/product/kartridzh-cactus-cs-ph7400-106r01080-chernyi-982045/</url><price>3200</price><oldprice>3712</oldprice><currencyId>RUR</currencyId><categoryId>77</categoryId><market_category>Компьютеры/Аксессуары и расходные материалы/Картриджи, тонеры, фотобарабаны</market_category><picture>https://cdn.citilink.ru/84n0N_wvMocuv4JBe42TNi2hJGWh2bCQ7Lg90jvJDj4/background:255:255:255/extend:1/gravity:sm/height:500/resizing_type:fit/width:500/plain/items/982045_v01_b.jpg?version=1700870400</picture><delivery>true</delivery><name>Картридж Cactus CS-PH7400, 106R01080, черный / CS-PH7400</name><vendor>CACTUS</vendor><vendorCode>CS-PH7400</vendorCode><description>черный, примерный ресурс(страниц)- 15000, для Phaser 7400</description><sales_notes>Клубные цены ещё ниже. Присоединяйтесь!</sales_notes><manufacturer_warranty>true</manufacturer_warranty><cpa>false</cpa><delivery-options><option cost=\"390\" days=\"2-3\" order-before=\"22\"></option></delivery-options><pickup-options><option cost=\"0\" days=\"34\"></option></pickup-options><barcode>4690207263313</barcode><param name=\"Цвет\">черный</param><param name=\"Group\">Group9</param><url2>https://www.citilink.ru/catalog/computers_and_notebooks/monitors_and_office/cartridges/982045/</url2><model>CS-PH7400</model><typePrefix>Картриджи</typePrefix></offer>"
+    //- тэг открывается -> записывается первое значение после скобки "<" (это по сути 1 оффер/элемент)
+    //- Идем дальше по тэгам
+    //- Далее проверяется - если закрывающий тэг не равно "/>" то вродолжает идти дальше по строке
+    // до следующего открывающего тэга "<"
+    // -Если первое значение следующего открывающего тэга не равно записонному значению то идем дальше
+    // по строке (записываем значение в модель).
+    // -Если первое значение следующего открывающего тэга равно /+записанное значение, то тэг закрывается
+
+
+    val mockFeed = "<offer id=\"982045\" available=\"true\">" +
+                        "<url>https://www.citilink.ru/product/kartridzh-cactus-cs-ph7400-106r01080-chernyi-982045/</url>" +
+                        "<price>3200</price>" +
+                        "<oldprice>3712</oldprice>" +
+                        "<currencyId>RUR</currencyId>" +
+                        "<categoryId>77</categoryId>" +
+                        "<market_category>Компьютеры/Аксессуары и расходные материалы/Картриджи, тонеры, фотобарабаны</market_category>" +
+                        "<picture>https://cdn.citilink.ru/84n0N_wvMocuv4JBe42TNi2hJGWh2bCQ7Lg90jvJDj4/background:255:255:255/extend:1/gravity:sm/height:500/resizing_type:fit/width:500/plain/items/982045_v01_b.jpg?version=1700870400</picture>" +
+                        "<delivery>true</delivery>" +
+                        "<name>Картридж Cactus CS-PH7400, 106R01080, черный / CS-PH7400</name>" +
+                        "<vendor>CACTUS</vendor>" +
+                        "<vendorCode>CS-PH7400</vendorCode>" +
+                        "<description>черный, примерный ресурс(страниц)- 15000, для Phaser 7400</description>" +
+                        "<sales_notes>Клубные цены ещё ниже. Присоединяйтесь!</sales_notes>" +
+                        "<manufacturer_warranty>true</manufacturer_warranty>" +
+                        "<cpa>false</cpa>" +
+                        "<delivery-options>" +
+                            "<option cost=\"390\" days=\"2-3\" order-before=\"22\"></option>" +
+                        "</delivery-options>" +
+                        "<pickup-options>" +
+                            "<option cost=\"0\" days=\"34\"></option>" +
+                        "</pickup-options>" +
+                        "<barcode>4690207263313</barcode>" +
+                        "<param name=\"Цвет\">черный</param>" +
+                        "<param name=\"Group\">Group9</param>" +
+                        "<url2>https://www.citilink.ru/catalog/computers_and_notebooks/monitors_and_office/cartridges/982045/</url2>" +
+                        "<model>CS-PH7400</model>" +
+                        "<typePrefix>Картриджи</typePrefix>" +
+                    "</offer>"
 //        "<offer id=\"982046\" available=\"true\"><url>https://www.citilink.ru/product/kartridzh-cactus-cs-ph7400c-106r01150-goluboi-982046/</url><price>2350</price><oldprice>2679</oldprice><currencyId>RUR</currencyId><categoryId>77</categoryId><market_category>Компьютеры/Аксессуары и расходные материалы/Картриджи, тонеры, фотобарабаны</market_category><picture>https://cdn.citilink.ru/UvlyhrOp5PNsdO2-Man0mrRPsru0Msj76XQJdFe_0eI/background:255:255:255/extend:1/gravity:sm/height:500/resizing_type:fit/width:500/plain/items/982046_v01_b.jpg?version=1700870400</picture><delivery>true</delivery><name>Картридж Cactus CS-PH7400C, 106R01150, голубой / CS-PH7400C</name><vendor>CACTUS</vendor><vendorCode>CS-PH7400C</vendorCode><description>голубой, примерный ресурс(страниц)- 9000, для Phaser 7400</description><sales_notes>Клубные цены ещё ниже. Присоединяйтесь!</sales_notes><manufacturer_warranty>true</manufacturer_warranty><cpa>false</cpa><delivery-options><option cost=\"390\" days=\"2-3\" order-before=\"22\"></option></delivery-options><pickup-options><option cost=\"0\" days=\"34\"></option></pickup-options><barcode>4690207263306</barcode><param name=\"Цвет\">голубой</param><param name=\"Group\">Group9</param><url2>https://www.citilink.ru/catalog/computers_and_notebooks/monitors_and_office/cartridges/982046/</url2><model>CS-PH7400C</model><typePrefix>Картриджи</typePrefix></offer>" +
 //        "<offer id=\"982047\" available=\"true\"><url>https://www.citilink.ru/product/kartridzh-cactus-cs-ph7400m-106r01151-purpurnyi-982047/</url><price>2410</price><oldprice>2675</oldprice><currencyId>RUR</currencyId><categoryId>77</categoryId><market_category>Компьютеры/Аксессуары и расходные материалы/Картриджи, тонеры, фотобарабаны</market_category><picture>https://cdn.citilink.ru/e3awP1V7dZaMK1ltMgh4FfA5t8ScXfXZwNeC6Qd9E2Q/background:255:255:255/extend:1/gravity:sm/height:500/resizing_type:fit/width:500/plain/items/982047_v01_b.jpg?version=1700870400</picture><delivery>true</delivery><name>Картридж Cactus CS-PH7400M, 106R01151, пурпурный / CS-PH7400M</name><vendor>CACTUS</vendor><vendorCode>CS-PH7400M</vendorCode><description>пурпурный, примерный ресурс(страниц)- 9000, для Phaser 7400</description><sales_notes>Клубные цены ещё ниже. Присоединяйтесь!</sales_notes><manufacturer_warranty>true</manufacturer_warranty><cpa>false</cpa><delivery-options><option cost=\"390\" days=\"2-3\" order-before=\"22\"></option></delivery-options><pickup-options><option cost=\"0\" days=\"34\"></option></pickup-options><barcode>4690207263290</barcode><param name=\"Цвет\">пурпурный</param><param name=\"Group\">Group10</param><url2>https://www.citilink.ru/catalog/computers_and_notebooks/monitors_and_office/cartridges/982047/</url2><model>CS-PH7400M</model><typePrefix>Картриджи</typePrefix></offer>" +
 //        "<offer id=\"982048\" available=\"true\"><url>https://www.citilink.ru/product/kartridzh-cactus-cs-ph7400y-106r01152-zheltyi-982048/</url><price>3200</price><oldprice>3648</oldprice><currencyId>RUR</currencyId><categoryId>77</categoryId><market_category>Компьютеры/Аксессуары и расходные материалы/Картриджи, тонеры, фотобарабаны</market_category><picture>https://cdn.citilink.ru/j9pgHN4lbySOUDC1jFZ7PToy_nXIfe04jgj_m5K0Tqg/background:255:255:255/extend:1/gravity:sm/height:500/resizing_type:fit/width:500/plain/items/982048_v01_b.jpg?version=1700870400</picture><delivery>true</delivery><name>Картридж Cactus CS-PH7400Y, 106R01152, желтый / CS-PH7400Y</name><vendor>CACTUS</vendor><vendorCode>CS-PH7400Y</vendorCode><description>желтый, примерный ресурс(страниц)- 9000, для Phaser 7400</description><sales_notes>Клубные цены ещё ниже. Присоединяйтесь!</sales_notes><manufacturer_warranty>true</manufacturer_warranty><cpa>false</cpa><delivery-options><option cost=\"390\" days=\"2-3\" order-before=\"22\"></option></delivery-options><pickup-options><option cost=\"0\" days=\"34\"></option></pickup-options><barcode>4690207263283</barcode><param name=\"Цвет\">желтый</param><param name=\"Group\">Group9</param><url2>https://www.citilink.ru/catalog/computers_and_notebooks/monitors_and_office/cartridges/982048/</url2><model>CS-PH7400Y</model><typePrefix>Картриджи</typePrefix></offer>" +
@@ -12,45 +48,68 @@ fun main() {
 //        "<offer id=\"982052\" available=\"true\"><url>https://www.citilink.ru/product/kartridzh-cactus-cs-ph7500y-106r01445-zheltyi-982052/</url><price>3300</price><oldprice>3828</oldprice><currencyId>RUR</currencyId><categoryId>77</categoryId><market_category>Компьютеры/Аксессуары и расходные материалы/Картриджи, тонеры, фотобарабаны</market_category><picture>https://cdn.citilink.ru/MI4tqCzu_BtK2sBB6CmrmVSGCV-d7wTiLQIGjFP2hJE/background:255:255:255/extend:1/gravity:sm/height:500/resizing_type:fit/width:500/plain/items/982052_v01_b.jpg?version=1700870400</picture><delivery>true</delivery><name>Картридж Cactus CS-PH7500Y, 106R01445, желтый / CS-PH7500Y</name><vendor>CACTUS</vendor><vendorCode>CS-PH7500Y</vendorCode><description>желтый, примерный ресурс(страниц)- 17800, для Phaser 7500</description><sales_notes>Клубные цены ещё ниже. Присоединяйтесь!</sales_notes><manufacturer_warranty>true</manufacturer_warranty><cpa>false</cpa><delivery-options><option cost=\"390\" days=\"2-3\" order-before=\"22\"></option></delivery-options><pickup-options><option cost=\"0\" days=\"34\"></option></pickup-options><barcode>4690207263245</barcode><param name=\"Цвет\">желтый</param><param name=\"Group\">Group9</param><url2>https://www.citilink.ru/catalog/computers_and_notebooks/monitors_and_office/cartridges/982052/</url2><model>CS-PH7500Y</model><typePrefix>Картриджи</typePrefix></offer>" +
 //        "<offer id=\"982068\" available=\"true\"><url>https://www.citilink.ru/product/kartridzh-struinyi-cactus-cs-lc529xlbk-chernyi-61ml-dlya-brother-dcp-j-982068/</url><price>450</price><oldprice>526</oldprice><currencyId>RUR</currencyId><categoryId>77</categoryId><market_category>Компьютеры/Аксессуары и расходные материалы/Картриджи, тонеры, фотобарабаны</market_category><picture>https://cdn.citilink.ru/kb0crMA1RZ1fIfOBVUuhgBNEGKWocaRV-r_Ccu2YlQk/background:255:255:255/extend:1/gravity:sm/height:500/resizing_type:fit/width:500/plain/items/982068_v01_b.jpg?version=1700870400</picture><delivery>true</delivery><name>Картридж Cactus CS-LC529XLBK, черный / CS-LC529XLBK</name><vendor>CACTUS</vendor><vendorCode>CS-LC529XLBK</vendorCode><description>черный, объем 61 мл, для DCP-J100/J105/J200</description><sales_notes>Клубные цены ещё ниже. Присоединяйтесь!</sales_notes><manufacturer_warranty>true</manufacturer_warranty><cpa>false</cpa><delivery-options><option cost=\"390\" days=\"2-3\" order-before=\"22\"></option></delivery-options><pickup-options><option cost=\"0\" days=\"34\"></option></pickup-options><barcode>4690207263207</barcode><param name=\"Цвет\">черный</param><param name=\"Group\">Group10</param><url2>https://www.citilink.ru/catalog/computers_and_notebooks/monitors_and_office/cartridges/982068/</url2><model>CS-LC529XLBK</model><typePrefix>Картриджи</typePrefix></offer>"
 
+    //- тэг открывается -> записывается первое значение после скобки "<" (это по сути 1 оффер/элемент)
+    //- Идем дальше по тэгам
+    //- Далее проверяется - если закрывающий тэг не равно "/>" то вродолжает идти дальше по строке
 
-//    val test = mockFeed.filter { it == '<' }.length
-//    val test = mockFeed.find { it == '<' }
-    val test = mockFeed.contains("<", ignoreCase = true)
+    var firstIndexElement = ""
+    var testIndex = ""
+    val offerModel = arrayListOf<String>()
 
-    var newArray = "ff"
-    if (mockFeed.contains("<"))
-    newArray+"ddd"
+    if (mockFeed.contains("<") && mockFeed.contains(">")){
+        testIndex += mockFeed.trim().split("<")[1].split(">")[0]
+        firstIndexElement = testIndex.split(" ")[0]
+    }
 
-    println(test)
-    println(newArray)
+    mockFeed.split("><").forEach {offerTag ->
+        var varTag = offerTag
+        if (varTag.first().toString() == "<") varTag = varTag.replace("<", "")
+        else if (varTag.last().toString() == ">") varTag = varTag.replace(">", "")
+        println("<$varTag>")
 
-    fun String.findWord(word: String) = "\\b$word\\b".toRegex().containsMatchIn(this)
-
-    "I like Java!".findWord("Java") // true
-    "I like JavaScript".findWord("Java") // false
+    }
 
 
+//    for (i in (mockFeed.contains("<") && mockFeed.contains(">")))
+//    if (mockFeed.contains("<") && mockFeed.contains(">")){
+//    println()
 
-//    fun execute(): List<NewsListModel> {
-//        val xmlNewModelList = mutableListOf<NewsListModel>()
-//        val responseTest = newsRepository.getNewsXml()
-//        var indexOfList = 0
-//        var title = ""
-//        var date = ""
-//        var description = ""
-//        var keywords = mutableListOf<String>()
-//        for (i in responseTest.execute().body()!!.charStream().readLines()){
-//            if (i.contains("title")){title = i.trim().split(">")[1].split("</")[0]}
-//            if (i.contains("date")){date = i.trim().split(">")[1].split("</")[0]}
-//            if (i.contains("description")){description = i.trim().split(">")[1].split("</")[0]}
-//            if (i.contains("<element>") && i.contains("</element>")){ keywords.add(i.trim().split(">")[1].split("</")[0]) }
-//            if (i.contains("/visible")) {
-//                xmlNewModelList.add(indexOfList, NewsListModel(date,description,0, keywords, title,true))
-//                indexOfList++
-//                keywords = mutableListOf()
-//            }
+//    if (mockFeed.contains("<") && mockFeed.contains(">")){
+//        testIndex += mockFeed.trim().split("<")[1].split(">")[0]
+//        testIndex += mockFeed.trim().split("<")[1].split(">")[0]
+//        println(testIndex)
+//    }
+
+
+//    val x1 = mockFeed.filter { i -> i == '<' }.length
+//    for (i in 1..x1){
+//        offerModel.add(mockFeed.split("<")[i].split(" ")[0])
+//    }
+//
+//    for (i in offerModel.subList(1,offerModel.size)) {
+//        if (!offerModel.last().contains(i)){
+//         println(i)
 //        }
-//        return xmlNewModelList
+//    }
+
+
+
+//    offerModel.forEach{
+//        println(offerModel.last().contains(it))
+////        println(offerModel[0].contains(offerModel.last()))
+//    }
+
+//    while (offerModel[0])
+
+
+
+//    println(offerModel)
+//    val firstElement = mockFeed.split("<")[1].split(" ")[0]
+//    val x2 = x1
+//    print(x1)
+//    println()
+//    x1.forEach {
+//        println(it)
 //    }
 
 }
