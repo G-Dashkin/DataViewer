a036ef28390976c432365b0e4768a00ab18f850d
George Dashkin <siodo@mail.ru>
2024-02-29 16:03:30 +0300

    12_01_2024

--- app/src/main/java/com/example/dataviewer/data/network/api/parser3/NewParser3.kt
+++ app/src/main/java/com/example/dataviewer/data/network/api/parser3/NewParser3.kt
@@ -25,65 +25,32 @@ class NewParser3 {
                     newElement = varTag.replace("/","")
 
                     if (oldElement == newElement) {
-                        if (feedElements.size < 1500) feedElements.set(feedElements.size-1, feedElements.last() + "<$varTag>")
+                        if (feedElements.size < 500) feedElements.set(feedElements.size-1, feedElements.last() + "<$varTag>")
                     } else {
-                        // Нужно доработать...
-//                        if(checkSameElements(feedElements, varTag)) feedElements.add("<$varTag>") else
 
-//                        println(feedElements.slice(feedElements.size - 9..feedElements.size - 1))
-//                        println(feedElements.contains(feedElements.last().split(" ")[0]+">" + "..."))
-                        if(checkSameElements(feedElements, varTag) && feedElements.size < 1500) {
+                        if(checkSameElement(feedElements, varTag) && feedElements.size < 500) {
                             feedElements.add("<$varTag>")
-//                        } else if() {
                         } else {
-//                            while ()
+                            feedElements.set(feedElements.size-1, feedElements.last().replace(">...", ""))
                             feedElements.set(feedElements.size-1, feedElements.last().split(" ")[0]+">" + "...")
                         }
-//                        if (feedElements.size < 1500) feedElements.add("<$varTag>")
                     }
                 }
-
             }
             return feedElements
         }
 
-        // Классная функция)))
-        fun checkSameElements(arrayFeed: ArrayList<String>, newElement: String): Boolean {
+        fun checkSameElement(arrayFeed: ArrayList<String>, addedElement: String): Boolean {
 
-            val newNewArray = ArrayList<String>()
-            val newNewElement = newElement.split(" ")[0]
+            var newNewArray = ArrayList<String>()
+            val newAddedElement = addedElement.split(" ")[0]
 
             if (arrayFeed.size >= 9) {
-
-                val newArray = arrayFeed.slice(arrayFeed.size - 9..arrayFeed.size - 1) as ArrayList
-
-                //----------------------------------------------------------------------------------
-                println("----------Массив без измененеий------------")
-                newArray.forEach { println(it) }
-                println("-------------------------------------------")
-                println("----------Передаваемый элемент-------------")
-                println(newNewElement)
-                println("$newNewElement>...")
-                println("-------------------------------------------")
-
-
-//                newArray.forEach { println(it.split(" ")[0].replace("<","")) }
-                newArray.add(newNewElement)
-
-                for (i in newArray) {
-                    newNewArray.add(i.split(" ")[0].replace("<",""))
-                }
+                newNewArray = arrayFeed.slice(arrayFeed.size - 9..arrayFeed.size - 1) as ArrayList
+                newNewArray.add(newAddedElement)
+                newNewArray.replaceAll { it.split(" ")[0].replace("<","").replace(">"," ").split(" ")[0] }
             }
-
-//            newNewArray.forEach { it }
-//            println(arrayFeed.last().split(" ")[0].replace("<","")+">" + "...")
-//            newNewArray.remove(newNewArray.last().split(" ")[0]+">" + "...".replace("<",""))
-//            newNewArray.forEach { println(it.split(" ")[0].replace("<","")) }
-            println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-            println("size != 1: " + (newNewArray.toSet().size != 1).toString())
-            println("!contains: " + !newNewArray.contains("$newNewElement>..."))
-            println("    Общее: ${(newNewArray.toSet().size != 1) && (!newNewArray.contains("$newNewElement>..."))}")
-            return (newNewArray.toSet().size != 1) && (!newNewArray.contains("$newNewElement>..."))
+            return newNewArray.toSet().size != 1
         }
 
     }
