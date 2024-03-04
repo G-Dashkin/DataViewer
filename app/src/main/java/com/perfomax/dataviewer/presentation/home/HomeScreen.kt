0bed28d47664e79fdb3a0f6880c31c3a66ac7097
= <=>
2024-03-04 04:42:45 +0300

    new commit 03_03_2024

--- app/src/main/java/com/perfomax/dataviewer/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/perfomax/dataviewer/presentation/home/HomeScreen.kt
@@ -1,19 +1,22 @@
 package com.perfomax.dataviewer.presentation.home
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.perfomax.dataviewer.core.ui.theme.DataViewerTheme
 
 @Composable
 fun HomeScreen() {
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().border(1.dp, Color.Red),
         contentAlignment = Alignment.Center
     ) {
         Text(text = "HomeScreen", fontSize = 22.sp)
