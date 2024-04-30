e989bad1f6e5f17720d9a8299135da4183165d8d
= <=>
2024-05-01 01:14:49 +0300

    new commit 30_04_2024

--- presentation/src/main/java/com/perfomax/dataviewer/presentation/feeds/FeedsViewModel.kt
+++ presentation/src/main/java/com/perfomax/dataviewer/presentation/feeds/FeedsViewModel.kt
@@ -51,7 +51,7 @@ class FeedsViewModel @Inject constructor(
             currentState.copy(
 //                feedUrl = "https://citilink.ru"
 //                feedUrl = "citilink.в"
-                feedUrl = "https://feeds-mic.s1.citilink.ru/yandex_offer/ufa_cl.xml"
+                feedUrl = "https://feeds-mic.s1.citilink.ru/yandex_offer/spb_cl.xml"
 //                feedUrl = "https://feeds-mic.s1.citilink.ru/yandex_offer/spb_cl.xml"
 //                feedUrl = "https://api2.kiparo.com/static/it_news.xml"
             )
